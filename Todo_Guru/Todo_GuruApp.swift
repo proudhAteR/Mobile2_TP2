f77ccebc1a848e7045750bf2ac8362a6f098b5e4
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2025-02-26 01:14:47 -0500

    Initial Commit

---
+++ Todo_Guru/Todo_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_GuruApp.swift
+//  Todo_Guru
+//
+//  Created by hater__ on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Todo_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
